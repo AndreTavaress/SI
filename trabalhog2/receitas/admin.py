0e3b89d4775455a41aabd02da9c3fed2ab4b2ad2
AndreTavaress <96271747+AndreTavaress@users.noreply.github.com>
2024-11-18 11:15:16 -0300

    commit_03

--- trabalhog2/receitas/admin.py
+++ trabalhog2/receitas/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from receitas.models import *
 
-# Register your models here.
+@admin.register(receita)
+class ReceitaAdmin(admin.ModelAdmin):
+    list_display = ('ingredientes','receita_criada')
--- trabalhog2/receitas/models.py
+++ trabalhog2/receitas/models.py
@@ -1,2 +1,8 @@
 from django.db import models
 
+class receita(models.Model):
+    ingredientes = models.CharField(max_length=1000)
+    receita_criada = models.CharField(max_length= 10000)
+    
+    def __str__(self) -> str:
+        return f"Receita: {self.id}"
